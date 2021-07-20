5dec6385329e80c7661742006c29e2e136348639
Jorge Viera <Jorge5psgt@gmail.com>
2021-07-19 22:46:14 -0400

    starts

---
+++ client/pages/SearchMarinas.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import axios from 'axios';
+import MarinasComponents from './MarinasComponents'
+// import '.client/utils/api.js'
+const data = "data."
+const SearchMarinas = () => {
+
+    const [marinas, setMarinas] = useState([]);
+    const [search, setSearch] = useState('');
+    console.log("Marina", marinas);
+    console.log("marina data", marinas.data);
+
+
+    useEffect(() => {
+        axios.get(`https://api.marinas.com//v1/points/search`)
+            .then(res => {
+                setMarinas(res.data);
+            }).catch(err => console.log(err, 'error in setMarinas useEffect'))
+    }, [search])
+
+    const handleChange = async e => {
+        e.preventDefault();
+        setSearch(e.target.value);
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        // setSearch(e.target.value)
+    }
+
+    const handleKeypress = e => {
+        e.preventDefault();
+        //it triggers by pressing the enter key
+        if (e.keyCode === 13) {
+            handleSubmit();
+        }
+    };
+
+    return (
+        <div>
+            <Jumbotron fluid className='text-light bg-primary'>
+                <Container>
+                    <h1>Search for a Marina</h1>
+                    <Form /*onSubmit={handleFormSubmit}*/>
+                        <Form.Row>
+                            <Col xs={12} md={8}>
+                                <Form.Control
+                                    name='searchInput'
+                                    /*value={searchInput}*/
+                                    onChange={handleChange}
+                                    onKeyPress={handleKeypress}
+                                    type='text'
+                                    size='lg'
+                                    placeholder='Search for a Marina'
+                                />
+                            </Col>
+                            <Col xs={12} md={4}>
+                                <Button type='submit' variant='dark' size='lg'>
+                                    Click to Search
+                                </Button>
+                            </Col>
+                        </Form.Row>
+                    </Form>
+                    <div>
+                        {marinas.length}
+                        <div className="comp-card">
+                <MarinasComponents
+                  key= {data.image}
+                  name= {data.name}
+               
+              />
+              </div>
+                    </div>
+            </Container>
+        </Jumbotron>
+    </div>
+    )
+}
+
+export default SearchMarinas
